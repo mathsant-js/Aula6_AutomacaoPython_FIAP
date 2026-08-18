d90ea8553434dc13d0ec831356ed85632ad7a694
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2026-08-18 19:07:10 -0300

    feat: alterando o modo de fazer

--- desafio/main.py
+++ desafio/main.py
@@ -1,35 +1,21 @@
-def contar_dominios(emails):
-    dicionario = dict()
-    
-    for email in emails:
-        _, dominio = email.split("@")
-        
-        if dominio not in dicionario:
-            dicionario[dominio] = 1
-        else:
-            dicionario[dominio] += 1
-            
-    return dicionario
-
-def exibir_email_por_chave(chave, dicionario):
-    if chave in dicionario:
-        for email in emails_separados:
-            nome_usuario, dominio = email.split("@")
-            
-            if dominio == chave:
-                print(nome_usuario, end=", ")
-                         
-                
-emails = "joao.silva@fiap.com.br, maria.souza@fiap.com.br, ana.paula@fiap.com.br, rodrigo.lima@alun.com.br"
+emails = "joao.silva@fiap.com.br, maria.souza@fiap.com.br, ana.paula@fiap.com.br, rodrigo.lima@alun.com.br, artur.rosa@alun.com.br"
 
 emails_separados = emails.replace(" ", "").split(",")
 
-dicionario = contar_dominios(emails_separados)
+usuarios_por_dominio = {}
 
-for chave, dados in dicionario.items():
+for email in emails_separados:
+    usuario, dominio = email.split("@")
+    
+    if dominio not in usuarios_por_dominio:
+        usuarios_por_dominio[dominio] = []
+        
+    usuarios_por_dominio[dominio].append(usuario)
+
+for dominio, usuarios in usuarios_por_dominio.items():
+    usuarios = tuple(usuarios)
+    
     print("Quantidade de emails por domínio:")
-    print(f"{chave}: {dados}")
-    print("Lista de Usuários: ", end="")
-    exibir_email_por_chave(chave, dicionario)
-    print()
+    print(f"{dominio}: {len(usuarios)}")
+    print(f"Lista de usuários: {usuarios}")
     print()
