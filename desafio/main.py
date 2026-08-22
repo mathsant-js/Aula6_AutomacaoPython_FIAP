a01317e94f9f2214a352ee92a9344cfea7d15f2d
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2026-08-22 18:53:20 -0300

    feat: desafio concluído

--- desafio/main.py
+++ desafio/main.py
@@ -4,6 +4,11 @@ emails_separados = emails.replace(" ", "").split(",")
 
 usuarios_por_dominio = {}
 
+def trocar_primeiro_ultimo(lista: list) -> list:
+    lista[0], lista[-1] = lista[-1], lista[0]
+    
+    return lista
+
 for email in emails_separados:
     usuario, dominio = email.split("@")
     
@@ -13,18 +18,12 @@ for email in emails_separados:
     usuarios_por_dominio[dominio].append(usuario)
 
 for dominio, usuarios in usuarios_por_dominio.items():
-    usuarios_tupla = tuple(usuarios)
-
-    primeira_posicao = 0
-    ultima_posicao = len(usuarios_tupla) - 1
-
-    primeiro_usuario = usuarios_tupla[primeira_posicao]
-    ultimo_usuario = usuarios_tupla[ultima_posicao]
-
-    primeiro_usuario, ultimo_usuario = ultimo_usuario, primeiro_usuario
-    
     print("Quantidade de emails por domínio:")
     print(f"{dominio}: {len(usuarios)}")
-    print(f"Lista de usuários: {usuarios}")
-    print(f"Após troca de posições: {primeiro_usuario}, {usuarios_tupla[1:ultima_posicao]}, {ultimo_usuario}")
+    print(f"Lista de usuários: {tuple(usuarios)}")
+    
+    usuarios = trocar_primeiro_ultimo(usuarios)
+    usuarios_tupla = tuple(usuarios)
+    
+    print(f"Após troca de posições: {usuarios_tupla}")
     print()
